a3431352c6fee65813eba3cbfadd244368314ab2
shekhrupon <shekhrupon@gmail.com>
2019-12-07 22:45:08 +0600

    Ecommerce project

---
+++ resources/views/backEnd/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="row-fluid">
+    <div id="footer" class="span12">Developed by Bit Legion</div>
+</div>
---
+++ resources/views/frontEnd/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer id="footer"><!--Footer-->              
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <p class="pull-left">Copyright © 2019 MOBILE SHOP Inc. All rights reserved.</p>
+                <p class="pull-right">Designed by <span><a target="_blank" href="#">Bit Legion</a></span></p>
+            </div>
+        </div>
+    </div>
+</footer><!--/Footer-->
